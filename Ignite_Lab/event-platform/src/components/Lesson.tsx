f26aaeef34372f26b99a9fbd71c2d25eb682db4f
LucianoNeo <tec.info.luciano@hotmail.com>
2022-06-23 20:49:33 -0300

    dia 04 finalizado

--- Ignite_Lab/event-platform/src/components/Lesson.tsx
+++ Ignite_Lab/event-platform/src/components/Lesson.tsx
@@ -1,7 +1,10 @@
 import {CheckCircle } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classNames'
+
+
 interface LessonProps{
     title:string;
     slug:string;
@@ -13,8 +16,12 @@ interface LessonProps{
 
 export function Lesson(props:LessonProps){
 
+const { slug } = useParams<{slug:string}>()
+
 const isLessonAvailable = isPast(props.availableAt)
 const availableDateFormatted = format(props.availableAt, "EEEE ' • ' d ' de ' MMMM' • ' k'h'mm",{locale: ptBR})
+
+const isActiveLesson = slug === props.slug
 
     return(
         <Link to={`/event/lesson/${props.slug}`} className='group'>
@@ -22,10 +29,19 @@ const availableDateFormatted = format(props.availableAt, "EEEE ' • ' d ' de ' 
             {availableDateFormatted}
             </span>
 
-            <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+            <div 
+            className={classNames('rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500',{
+                'bg-green-500': isActiveLesson,
+            })}>
                 <header className="flex items-center justify-between">
                     {isLessonAvailable ? (
-                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+
+                        <span className={classNames('text-sm font-medium flex items-center gap-2',{
+                            'text-white': isActiveLesson,
+                            'text-blue-500': !isActiveLesson,
+
+                        })}
+                        >
                         <CheckCircle size={20}/>
                         Conteúdo liberado
                          </span>
@@ -35,12 +51,20 @@ const availableDateFormatted = format(props.availableAt, "EEEE ' • ' d ' de ' 
                         Em breve
                          </span>
                     )}
-                    <span className="text-xs rounded py-{0.125rem} p-2 text-white border border-green-300 font-bold" >
+
+
+                    <span className={classNames("text-xs rounded py-{0.125rem} p-2 text-white border border-green-300 font-bold",{
+                      'border-white'  : isActiveLesson,
+                      'border-green-300': !isActiveLesson
+                    })} >
                         {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
                     </span>
                 </header>
 
-                <strong className="text-gray-200 mt-5 block">
+                <strong className={classNames(' mt-5 block',{
+                    'text-white': isActiveLesson,
+                    'text-gray-200': !isActiveLesson
+                })}>
                     {props.title}
                 </strong>
             </div>
--- Ignite_Lab/event-platform/src/lib/apollo.ts
+++ Ignite_Lab/event-platform/src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-uri: "https://api-sa-east-1.graphcms.com/v2/cl4ng6j1e03h601z007e6di37/master",
+uri: import.meta.env.VITE_API_URL,
+headers: {
+    'Authorization':  `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+},
 cache: new InMemoryCache()
 })
