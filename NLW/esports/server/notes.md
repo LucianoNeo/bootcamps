50e56b6c479d43c8b7287c45394c47314c6b1f37
LucianoNeo <tec.info.luciano@hotmail.com>
2022-09-14 17:41:33 -0300

    backend developed

--- NLW/esports/server/notes.md
+++ NLW/esports/server/notes.md
@@ -19,3 +19,19 @@
 ## Instalar TSNODEDEV
 
 ```npm install ts-node-dev -D ``` // Já faz a transpilação e auto refresh do typescript
+
+## Instalar ORM Prisma com SQLITE
+``` npx prisma init --datasource-provider sqlite ```
+
+## Gerar o banco
+npx prisma migrate dev
+
+## Gerar interface grafica
+npx prisma studio
+
+## Instalar o client do PRISMA
+npm i @prisma/client
+
+## Instalar CORS
+npm i cors
+npm i @types/cors -D
--- NLW/esports/server/src/server.ts
+++ NLW/esports/server/src/server.ts
@@ -1,14 +1,103 @@
-import express from 'express'
+import express, { response } from 'express'
+import { PrismaClient } from '@prisma/client'
+import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes'
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string'
+import cors from 'cors'
+
+const prisma = new PrismaClient({
+    log: ['query']
+})
 
 const app = express()
 
-app.get('/ads', (req, res) => {
-    return res.json([
-        { id: 1, name: 'Anuncio 1' },
-        { id: 2, name: 'Anuncio 2' },
-        { id: 3, name: 'Anuncio 3' },
-    ])
+app.use(express.json())
+
+app.use(cors())
+
+app.get('/games', async (req, response) => {
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true
+                }
+            }
+        }
+    })
+    return response.json(games)
 })
+
+
+app.post('/games/:id/ads', async (req, response) => {
+    const gameId = req.params.id
+    const body: any = req.body
+    
+    const ad = await prisma.ad.create({
+        data:{
+            gameId,
+            name: body.name,
+            yearsPlaying: body.yearsPlaying,
+            discord: body.discord,
+            weekDays: body.weekDays.join(','),
+            hourStart: convertHourStringToMinutes(body.hourStart),
+            hourEnd: convertHourStringToMinutes(body.hourEnd),
+            useVoiceChannel: body.useVoiceChannel,
+        }
+    })
+
+    return response.status(201).json(ad)
+
+})
+
+
+
+app.get('/games/:id/ads', async (req, response) => {
+    const gameId = req.params.id
+
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true,
+            yearsPlaying: true,
+            hourStart: true,
+            hourEnd: true,
+        },
+        where: {
+            gameId,
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+    return response.json(ads.map(ad => {
+        return {
+            ...ad,
+            weekDays: ad.weekDays.split(','),
+            hourStart: convertMinutesToHourString(ad.hourStart),
+            hourEnd: convertMinutesToHourString(ad.hourEnd)
+        }
+
+    }))
+})
+
+app.get('/ads/:id/discord', async (req, response) => {
+    const adId = req.params.id
+
+    const ad = await prisma.ad.findUniqueOrThrow({
+        select: {
+            discord: true
+        },
+        where: {
+            id: adId
+        }
+    })
+    return response.json({
+        discord: ad.discord,
+    })
+})
+
 
 app.listen(3333)
 console.log('Server rodando na porta 3333')
