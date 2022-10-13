1296b18ea7afa55431c660d22e8f30668dc3b6cf
LucianoNeo <tec.info.luciano@hotmail.com>
2022-10-13 11:15:33 -0300

    deploy storybook

--- Ignite_Lab/design-system/.storybook/main.cjs
+++ Ignite_Lab/design-system/.storybook/main.cjs
@@ -14,5 +14,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType == "PRODUCTION") {
+      config.base = '/bootcamps/Ignite_Lab/design-system/'
+    }
+    return config
   }
 }
