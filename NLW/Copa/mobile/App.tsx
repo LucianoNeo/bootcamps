956d502d864bc35bf3c52a0029f9712fc61a99e4
LucianoNeo <tec.info.luciano@hotmail.com>
2022-10-31 21:54:43 -0300

    primeiro dia finalizado

--- NLW/Copa/mobile/App.tsx
+++ NLW/Copa/mobile/App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Roboto_400Regular, Roboto_500Medium, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
+import { THEME } from './src/styles/theme';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ NLW/Copa/mobile/src/screens/SignIn.tsx
@@ -0,0 +1,12 @@
+import { Center, Text } from 'native-base'
+
+
+export function SignIn() {
+    return (
+        <Center flex={1} bgColor="gray.900">
+            <Text color="white" fontSize={24} >
+                Welcome to World CUP!
+            </Text>
+        </Center>
+    )
+}
